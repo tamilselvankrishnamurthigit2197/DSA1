8b13eca7ccc1b1fc851cec1ae2d1b4cd500e4eb6
tamilselvankrishnamurthigit2197 <tamilselvanmoorthi97@gmail.com>
2025-07-20 16:10:13 +0530

    bubble sort is added

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,15 @@
+function BubbleSort(arr) {
+
+    let n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n - i -1; j++) {
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+        
+    }
+    return arr;
+}
+console.log(BubbleSort([5, 3, 4, 6, 2, 8]));
